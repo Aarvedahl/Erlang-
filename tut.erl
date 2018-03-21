6256e08ae58fb79a1b0d1ca68c83a67d0519d86e
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-03-21 21:29:40 +0100

    Added division

--- tut.erl
+++ tut.erl
@@ -5,7 +5,7 @@
 
 -import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/1]).
 
--export([double/1, add/2, add/3]).
+-export([double/1, add/2, add/3, divide/2]).
 
 double(X) ->
     2 * X.
@@ -15,3 +15,6 @@ add(A, B) ->
 
 add(A, B, C) ->
     A + B + C.
+
+divide(A, B) ->
+    A /B.
